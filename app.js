c584b4e390709c855ad9bc3878d7e04e5cc870e6
ali <alicode0043@gmail.com>
2023-08-30 22:50:07 +0530

    with login authentication

--- app.js
+++ app.js
@@ -1,36 +1,75 @@
 //jshint esversion:6
 
+// require('dotenv').config();
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyParser=require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-find-or-create')
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-mongoose.connect("mongodb://127.0.0.1:27017/blog");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
-const postSchema ={
+
+app.use(session({
+  secret: 'our little secret',
+  resave: false,
+  saveUninitialized: false,
+  
+}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/blog");
+
+app.use(passport.initialize());
+  app.use(passport.session());
+
+
+
+const postSchema =new mongoose.Schema({
   title:String,
   content:String
-}
+})
+
+
+// postSchema.plugin(passportLocalMongoose);
+// postSchema.plugin(findOrCreate);
 
 const Post=mongoose.model('post', postSchema);
 
-const post1=new Post({
-  title:"DBMS",
-  content:"dbms is very usefull topic to learn"
-})
+
+
+
+const userSchema=new mongoose.Schema({
+  email:String,
+  password:String
+});
+
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User=new mongoose.model("User" ,userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+// const post1=new Post({
+//   title:"DBMS",
+//   content:"dbms is very usefull topic to learn"
+// })
 // post1.save();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+
 
 let posts = [];
 
@@ -49,27 +88,77 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-app.get("/compose", function(req, res){
-  res.render("compose");
+// app.get("/compose", function(req, res){
+//   res.render("compose");
+// });
+
+app.get("/compose", (req, res) =>{
+  if(req.isAuthenticated()){
+      res.render("compose");
+  }
+  else{
+      res.redirect("/login");
+  }
 });
 
+
+
 app.post("/compose", function(req, res){
-  // const post = {
-  //   title: req.body.postTitle,
-  //   title: req.body.postTitle,
-  // }
-  
+
   var compose=new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
   })
-  // posts.push(post);
 
   compose.save();
 
   res.redirect("/");
 
 });
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+app.post('/login', async (req, res) =>{
+  const user = new User({
+     username:req.body.username,
+     passport:req.body.passport
+  });
+  req.logIn(user, function(err){
+     if(err){
+         console.log(err);
+     }
+     else{
+         passport.authenticate("local")(req,res, function(){
+             res.redirect("/compose");
+         });
+     }
+  });
+});
+
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+
+app.post('/register', async (req, res) =>{
+    
+  User.register({username:req.body.username}, req.body.password, function(err,user){
+      if(err){
+          console.log(err);
+          res.redirect('/register');
+      }
+      else{
+          passport.authenticate("local")(req,res, function(){
+              res.redirect("/compose");
+          });
+      }
+  });
+  
+
+})
+
 
 app.get("/posts/:postName", async function(req, res){
   const requestedTitle = _.lowerCase(req.params.postName);
@@ -88,6 +177,14 @@ app.get("/posts/:postName", async function(req, res){
     }
 
   
+
+
+    // login page
+
+
+    
+
+
 
 
 });
